b03e550ca3b9c9418d94d47a8511448ed3e9162c
Sarah Yu <sarah.m.yu88@gmail.com>
2015-09-16 13:25:28 -0700

    Refactor to pass data from parent component to children

--- client/components/TVStream.js
+++ client/components/TVStream.js
@@ -3,26 +3,11 @@ var TVList = require('./TVList.js');
 var data = require('../../data/data.js');
 
 var TVStream = React.createClass({
-
-        getInitialState: function() {
-          return { data: [
-              {id: 1, show: "Show1"},
-              {id: 2, show: "Show2"},
-              {id: 3, show: "Show3"},
-              {id: 4, show: "Show4"}
-            ]};
-        },
-
-        componentDidMount: function() {
-          //add data from server here
-          // $.get
-        },
-
         render: function() {
-          var rows = this.state.data.map(function(show, i){
+          var rows = this.props.data.map(function(show, i){
             return <TVList data={show} key={show.id}></TVList> 
           })
-          return <div className="ui segments ten wide column">{rows}</div>          
+          return <div data={this.props.data} className="ui segments ten wide column">{rows}</div>          
         }
       });
 
--- client/components/firstComponent.js
+++ client/components/firstComponent.js
@@ -1,18 +1,49 @@
 var React = require('react');
 var TVStream = require('./TVStream.js');
 var TVSidebar = require('./TVSidebar.js');
+var TVData = require('../../data/data.js');
 
 var First = React.createClass({
-        render: function() {
+      getInitialState: function() {
+        return { stations: [
+            {station: "Station 1"},
+            {station: "Station 2"},
+            {station: "Station 3"},
+            {station: "Station 4"}
+          ],
+          shows: [
+            {id: 1, show: "Show1"},
+            {id: 2, show: "Show2"},
+            {id: 3, show: "Show3"},
+            {id: 4, show: "Show4"}
+          ]
+        };
+      },
+
+      componentDidMount: function() {
+        //add stations from server here
+        // $.get
+        if(this.isMounted()) {
+          var newStations = [];
+          for(var key in TVData) {
+            newStations.push({station: key});
+          }
+          this.setState({
+            stations: newStations
+          });
+        }
+      },
+
+      render: function() {
           return (
             <div className="ui grid">
-              <div className="row">
-                <TVStream></TVStream>
-                <TVSidebar></TVSidebar>
+            <div className="row">
+              <TVStream data={this.state.shows}></TVStream>
+              <TVSidebar data={this.state.stations}></TVSidebar>
               </div>
             </div>
             );
         }
-      });
+    });
 
 module.exports = First;
