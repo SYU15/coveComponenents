3d165f7f82e70f2e9a5e8c4d9ba6d7d5ac7b46e9
Sarah Yu <sarah.m.yu88@gmail.com>
2015-09-18 14:20:14 -0700

    Add README instructions

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # coveComponenents
+
+## About
+An extendable library of React Components ingesting KQED's media-api-service
+
+## Requirements
+- Node.js
+- npm
+- bower
+- gulp
+- nodemon
+
+## Getting Started
+- Clone/Fork the repo
+- ``npm install``
+- ``bower install``
+- ``gulp`` 
+  - This starts the browserify watch task and also starts up the server
+  - browserify task watches for changes in the client folder and compiles the JSX from react into a bundle.js file in the public folder 
+  - Must hit CTL + C twice to kill both processes in terminal
+- Go to localhost:8000 in your browser
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,13 @@ var browserify = require('browserify');
 var reactify = require('reactify');
 //must convert a string to a stream to allow browerify to be compatible with gulp
 var source = require('vinyl-source-stream');
+var nodemon = require('gulp-nodemon');
+var lreload = require('livereactload');
+var watchify = require('watchify');
 
 gulp.task('browserify', function() {
+  // lreload.monitor('public/bundle.js', {displayNotification: true});
+
   //entrypoint into app
     browserify('./client/App.js')
     //converts JSX to javascript
@@ -17,7 +22,15 @@ gulp.task('browserify', function() {
       .pipe(gulp.dest('public/'));
 });
 
-gulp.task('default',['browserify'], function() {
+gulp.task('serverwatch', function() {
+  nodemon({script: 'server.js', ext: 'js', ignore: ['gulpfile.js', 'public/bundle.js', 'node_modules/*', 'bower_components/*', '__tests__']})
+  .on('change', [])
+  .on('restart', function() {
+    console.log('Server has restarted');
+  });
+});
+
+gulp.task('default',['browserify', 'serverwatch'], function() {
   //watch all files immediately, rerun gulp tasks when there is a change
-    return gulp.watch('client/**/*.*', ['browserify'])
+    return gulp.watch('client/**/*.*', ['browserify', 'serverwatch']);
 });
