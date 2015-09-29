34ffb8f3e35d70e30c8f7037a5f0c5bb4c3e29c9
Sarah Yu <sarah.m.yu88@gmail.com>
2015-09-28 17:12:42 -0700

    Start unit tests

---
+++ unitTests/__tests__/arrow-test.js
@@ -0,0 +1,15 @@
+// jest.dontMock('../../client/components/TVScheduleTab/arrow');
+
+// describe('TV Schedule Arrows', function() {
+//   var React = require('react/addons');
+//   var TestUtils = React.addons.TestUtils;
+//   var actions = require('../../client/actions/appActions.js');
+//   var Arrow;
+//   beforeEach(function() {
+//     Arrow = require('../../client/components/TVScheduleTab/arrow'); 
+//   });
+//   it('should exist', function() {
+//     var arrow = TestUtils.renderIntoDocument(<Arrow />);
+//     expect(TestUtils.isCompositeComponent(arrow)).toBeTruthy();
+//   });
+// });
---
+++ unitTests/__tests__/tv-schedule-tabs.js
@@ -0,0 +1,14 @@
+// jest.dontMock('../../client/components/TVScheduleTab/TVTabWrapper');
+
+// describe('TV Schedule Tabs', function(){
+//   var React = require('react/addons');
+//   var TestUtils = React.addons.TestUtils;
+//   var Tabs;
+//   beforeEach(function() {
+//     Tabs = require('../../client/components/TVScheduleTab/TVTabWrapper'); 
+//   });
+//   it('should exist', function() {
+//     var tab = TestUtils.renderIntoDocument(<Tabs />);
+//     expect(TestUtils.isCompositeComponent(tab)).toBeTruthy();
+//   });
+// });
