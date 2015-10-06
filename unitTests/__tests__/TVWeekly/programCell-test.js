4928881416d87dffcaa131bde348f8a353163dd9
Sarah Yu <sarah.m.yu88@gmail.com>
2015-10-06 15:36:23 -0700

    Add tests for programCell

--- unitTests/__tests__/TVWeekly/programCell-test.js
+++ unitTests/__tests__/TVWeekly/programCell-test.js
@@ -6,19 +6,20 @@ describe('Program Cell for Weekly Schedule', function() {
   var ProgramCell;
   var React;
   var TestUtils;
-  var data = {
-    minutes: 30,
-    start_time: "0000",
-    episode_title: "Test Episode Title",
-    timestamp: 1444114800,
-    title: "Test Title",
-    episode_description: "Test episode description",
-    description: "Test description"
-  };
+  var data;
   beforeEach(function() {
     React = require('react/addons');
     TestUtils = React.addons.TestUtils;
-    ProgramCell = require('../../../client/components/TVWeekly/programCell'); 
+    ProgramCell = require('../../../client/components/TVWeekly/programCell');
+    data = {
+      minutes: 30,
+      start_time: "0000",
+      episode_title: "Test Episode Title",
+      timestamp: 1444114800,
+      title: "Test Title",
+      episode_description: "Test episode description",
+      description: "Test description"
+    }; 
   });
   
   it('should exist', function() {
@@ -54,7 +55,28 @@ describe('Program Cell for Weekly Schedule', function() {
     expect(description[0].props.children).toEqual('Test episode description');
     expect(dateFormat[0].props.children).toEqual('10/06/2015');
 
+  });
+
+  it('should shorten a description if the block is less than 30 minutes and description is longer than 50 characters', function(){ 
+   data.minutes = 25;
+   data.episode_title = 'Test episode description. Test, test, test. This is a really long description that will need to be shortened.';
+
+   var programCell = TestUtils.renderIntoDocument(<ProgramCell data={data} />);
+   var showDescription = TestUtils.scryRenderedDOMComponentsWithClass(programCell, 'react-small-description');
+
+   expect(showDescription[0].getDOMNode().textContent).toEqual('Test episode description. Test, test, test. This i...');
 
   });
+  
+  it('should hide an episode description if time block 15 minutes or less', function(){ 
+   data.minutes = 15;
+
+   var programCell = TestUtils.renderIntoDocument(<ProgramCell data={data} />);
+   var showDescription = TestUtils.scryRenderedDOMComponentsWithClass(programCell, 'react-small-description');
+
+   expect(showDescription[0].getDOMNode().textContent).toEqual('');
+
+  });
+
 
 });
